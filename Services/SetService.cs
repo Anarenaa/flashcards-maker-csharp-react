73cfa39eb95cd99ed3a7c3002d3d89e21d620cc2
Anastasiia Mikulych <mika.akima.22@gmail.com>
2026-03-21 21:31:51 +0200

    Modify Get methods

--- Services/SetService.cs
+++ Services/SetService.cs
@@ -10,23 +10,21 @@ namespace Services
         {
             _unitOfWork = unitOfWork;
         }
-        public async Task<List<Set>> GetAllSetsAsync(string? searchText = null)
+        public async Task<List<Set>> GetAllSetsAsync(List<int>? categoryIds, string? searchText = null)
         {
             var sets = await _unitOfWork.Sets.GetAllAsync(
                     filter: s => s.IsPublic
-                    && ((string.IsNullOrEmpty(searchText) 
-                            || s.Name.ToLower().Contains(searchText.ToLower())
-                        ))
+                    && (categoryIds == null || !categoryIds.Any() || s.Categories.Any(c => categoryIds.Contains(c.Id))) 
+                    && (string.IsNullOrEmpty(searchText) || s.Name.Contains(searchText))
             );
             return sets.ToList();
         }
-        public async Task<List<Set>> GetAllUserSets(int userId, string? searchText = null)
+        public async Task<List<Set>> GetAllUserSetsAsync(int userId, List<int>? categoryIds, string? searchText = null)
         {
             var sets = await _unitOfWork.Sets.GetAllAsync(
                     filter: s => s.UserId == userId
-                    && ((string.IsNullOrEmpty(searchText) 
-                        || s.Name.Contains(searchText) 
-                        || s.Description.Contains(searchText)))
+                    && (categoryIds == null || !categoryIds.Any() || s.Categories.Any(c => categoryIds.Contains(c.Id)))
+                    && (string.IsNullOrEmpty(searchText) || s.Name.Contains(searchText))
             );
             return sets.ToList();
         }
