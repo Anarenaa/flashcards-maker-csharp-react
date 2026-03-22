761111c68074ab1f5b017948d5f051294d803d91
HappyGh0st24 <pinchukdiana50@gmail.com>
2026-03-22 20:02:37 +0200

    Fix MySets

---
+++ App/Controllers/MySetsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services;
+
+namespace App.Controllers
+{
+    public class MySetsController : Controller
+    {
+        private readonly SetService _setService;
+        public MySetsController(SetService setService)
+        {
+            _setService = setService;
+        }
+        
+       
+        public async Task<IActionResult> Index()
+        {
+            var sets = await _setService.GetAllSetsAsync([]);
+
+            return View(sets);
+        }
+    }
+
+}
