0a122a32abfe986a2f64dc2df3f340d974f80165
Anastasiia Mikulych <mika.akima.22@gmail.com>
2026-03-23 15:08:54 +0200

    Add CollectionRepository with CRUD operations

---
+++ Repositories/CollectionRepository.cs
@@ -0,0 +1,21 @@
+﻿using Core.Context;
+using Core.Models;
+using Repositories.Interfaces;
+
+namespace Repositories
+{
+    public class CollectionRepository : Repository<Collection>, ICollectionRepository
+    {
+        public CollectionRepository(DataContext context) : base(context) { }
+        public async Task UpdateCollectionAsync(Collection collection)
+        {
+            var existingCollection = await _dbSet.FindAsync(collection.Id);
+            if (existingCollection is not null)
+            {
+                existingCollection.Name = collection.Name;
+                existingCollection.Description = collection.Description;
+                existingCollection.UpdatedAt = DateTime.UtcNow;
+            }
+        }
+    }
+}
