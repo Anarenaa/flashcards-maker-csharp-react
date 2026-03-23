4a6ab927fbc487799bc2e309fe05b6ac3bc352f9
Anastasiia Mikulych <mika.akima.22@gmail.com>
2026-03-23 14:22:12 +0200

    Add Category repository

---
+++ Repositories/CategoryRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Context;
+using Core.Models;
+using Repositories.Interfaces;
+
+namespace Repositories
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        public CategoryRepository(DataContext context) : base(context) { }
+        public async Task UpdateCategoryAsync(Category category)
+        {
+            var existingCategory = await _dbSet.FindAsync(category.Id);
+            if (existingCategory is not null)
+            {
+                existingCategory.Name = category.Name;
+            }
+        }
+    }
+}
