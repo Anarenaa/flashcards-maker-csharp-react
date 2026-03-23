5a10a3c9bc384cc65da37c3be210c7f8b737495f
Anastasiia Mikulych <mika.akima.22@gmail.com>
2026-03-23 19:56:03 +0200

    Optimize data loading for Collection service

--- Repositories/CollectionRepository.cs
+++ Repositories/CollectionRepository.cs
@@ -1,5 +1,6 @@
 ﻿using Core.Context;
 using Core.Models;
+using Microsoft.EntityFrameworkCore;
 using Repositories.Interfaces;
 
 namespace Repositories
@@ -16,6 +17,23 @@ namespace Repositories
                 existingCollection.Description = collection.Description;
                 existingCollection.UpdatedAt = DateTime.UtcNow;
             }
+        }
+        public async Task<bool> AnySetInCollectionAsync(int collectionId, int setId)
+        {
+            return await _dbSet
+                .Where(c => c.Id == collectionId)
+                .AnyAsync(c => c.Sets.Any(s => s.Id == setId));
+        }
+        public async Task<Set?> LoadSingleSetAsync(Collection collection, int setId)
+        {
+            var query = _context.Entry(collection)
+                .Collection(c => c.Sets)
+                .Query()
+                .Where(s => s.Id == setId);
+
+            await query.LoadAsync();
+
+            return await query.FirstOrDefaultAsync();
         }
     }
 }
