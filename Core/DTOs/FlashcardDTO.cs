8b4c226f051fa7954d21e4c9d723586d0c8c7a50
Anastasiia Mikulych <mika.akima.22@gmail.com>
2026-03-23 14:15:08 +0200

    Add FlashcardService with basic operations

---
+++ Core/DTOs/FlashcardDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.DTOs
+{
+    public class FlashcardDTO
+    {
+        public int? Id { get; set; }
+        public required string Term { get; set; }
+        public required string Definition { get; set; }
+    }
+}
---
+++ Services/FlashcardService.cs
@@ -0,0 +1,69 @@
+﻿using Core.DTOs;
+using Core.Exceptions;
+using Core.Models;
+using Repositories.Interfaces;
+
+namespace Services
+{
+    public class FlashcardService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public FlashcardService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<IEnumerable<FlashcardDTO>> GetFlashcardsBySetIdAsync(int setId)
+        {
+            var flashcards = await _unitOfWork.Flashcards.GetAllAsync(
+                filter: f => f.SetId == setId
+            );
+            return flashcards.Select(f => new FlashcardDTO
+            {
+                Id = f.Id,
+                Term = f.Term,
+                Definition = f.Definition
+            });
+        }
+        public async Task<FlashcardDTO> GetFlashcardByIdAsync(int id)
+        {
+            var flashcard = await _unitOfWork.Flashcards.GetByIdAsync(id);
+            if (flashcard == null)
+                throw new NotFoundException("Картка не знайдена.");
+            return new FlashcardDTO
+            {
+                Id = flashcard.Id,
+                Term = flashcard.Term,
+                Definition = flashcard.Definition
+            };
+        }
+        public async Task CreateFlashcardAsync(int setId, FlashcardDTO flashcardDto)
+        {
+            var flashcard = new Flashcard
+            {
+                Term = flashcardDto.Term,
+                Definition = flashcardDto.Definition,
+                SetId = setId
+            };
+            await _unitOfWork.Flashcards.AddAsync(flashcard);
+            await _unitOfWork.SaveChangesAsync();
+        }
+        public async Task UpdateFlashcardAsync(FlashcardDTO flashcardDto)
+        {
+            var flashcard = await _unitOfWork.Flashcards.GetByIdAsync(flashcardDto.Id.Value);
+            if (flashcard == null)
+                throw new NotFoundException("Картка не знайдена.");
+
+            await _unitOfWork.Flashcards.UpdateFlashcardAsync(flashcard);
+
+            await _unitOfWork.SaveChangesAsync();
+        }
+        public async Task DeleteFlashcardAsync(int id)
+        {
+            var flashcard = await _unitOfWork.Flashcards.GetByIdAsync(id);
+            if (flashcard == null)
+                throw new NotFoundException("Картка не знайдена.");
+            _unitOfWork.Flashcards.Delete(flashcard);
+            await _unitOfWork.SaveChangesAsync();
+        }
+    }
+}
