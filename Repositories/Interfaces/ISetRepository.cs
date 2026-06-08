e7849209a15afaabdbe3b1ffe69a68b90999bf33
Anastasiia Mikulych <mika.akima.22@gmail.com>
2026-06-08 13:16:49 +0300

    Revert

--- Repositories/Interfaces/ISetRepository.cs
+++ Repositories/Interfaces/ISetRepository.cs
@@ -6,6 +6,5 @@ namespace Repositories.Interfaces
     {
         Task<int> GetUserSetsCount(int userId);
         Task DeleteUserSets(int userId);
-        Task<Dictionary<int, float>> GetOverallProgressForSetsAsync(int userId, List<int> setIds);
     }
 }
--- Repositories/SetRepository.cs
+++ Repositories/SetRepository.cs
@@ -17,28 +17,5 @@ namespace Repositories
             var userSets = await _dbSet.Where(s => s.UserId == userId).ToListAsync();
             _dbSet.RemoveRange(userSets);
         }
-        public async Task<Dictionary<int, float>> GetOverallProgressForSetsAsync(int userId, List<int> setIds)
-        {
-            var totalCards = await _context.Flashcards
-                .Where(f => setIds.Contains(f.SetId))
-                .GroupBy(f => f.SetId)
-                .Select(g => new { SetId = g.Key, Total = g.Count() })
-                .ToListAsync();
-
-            var progressSums = await _context.CardProgresses
-                .Where(p => p.UserId == userId && setIds.Contains(p.Flashcard.SetId))
-                .GroupBy(p => p.Flashcard.SetId)
-                .Select(g => new { SetId = g.Key, ProgressSum = g.Sum(p => p.Progress) })
-                .ToListAsync();
-
-            var progressMap = progressSums.ToDictionary(x => x.SetId, x => x.ProgressSum);
-
-            return totalCards.ToDictionary(
-                x => x.SetId,
-                x => x.Total > 0
-                    ? (progressMap.GetValueOrDefault(x.SetId, 0f) / x.Total)
-                    : 0f
-            );
-        }
     }
 }
