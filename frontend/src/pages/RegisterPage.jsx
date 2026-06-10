edebaa6d26ece2bc94faa5f830e64dd19ffafbc5
Anastasiia Mikulych <mika.akima.22@gmail.com>
2026-06-10 21:41:12 +0300

    Add responsive design

--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -29,8 +29,7 @@ export default function RegisterPage() {
 
     if (!email.trim()) {
       validationErrors.Email = ["Введіть email"];
-    }
-    else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+    } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
       validationErrors.Email = ["Невірний формат email"];
     }
 
@@ -101,83 +100,93 @@ export default function RegisterPage() {
         <h1 className="title">Реєстрація</h1>
 
         <form onSubmit={handleSubmit} autoComplete="off">
-          <div className="form-group">
-            <input
-              placeholder="Вигадайте нікнейм"
-              name="userName"
-              className={hasError("UserName") ? "error-input" : ""}
-              type="text"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-            {errors.UserName && (
-              <span className="text-danger">{errors.UserName[0]}</span>
-            )}
-          </div>
-
-          <div className="form-group">
-            <input
-              type="text"
-              name="email"
-              className={hasError("Email") ? "error-input" : ""}
-              placeholder="example@mail.com"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            {errors.Email && (
-              <span className="text-danger">{errors.Email[0]}</span>
-            )}
-          </div>
-
-          <div className="separator">
-            <span></span>
-          </div>
-
-          <div className="form-group">
-            <div className="password-container">
-              <input
-                type={showPassword ? "text" : "password"}
-                name="password"
-                className={hasError("Password") ? "error-input" : ""}
-                placeholder="Пароль"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              <span
-                className="toggle-password"
-                onClick={() => setShowPassword(!showPassword)}
-              >
-                {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
-              </span>
-            </div>
-            {errors.Password && (
-              <span className="text-danger">{errors.Password[0]}</span>
-            )}
-          </div>
-
-          <div className="form-group">
-            <div className="password-container">
-              <input
-                type={showConfirmPassword ? "text" : "password"}
-                name="confirm"
-                className={hasError("ConfirmPassword") ? "error-input" : ""}
-                value={confirmPassword}
-                onChange={(e) => setConfirmPassword(e.target.value)}
-                placeholder="Повторіть пароль"
-              />
-              <span
-                className="toggle-password"
-                onClick={() => setShowConfirmPassword(!showConfirmPassword)}
-              >
-                {showConfirmPassword ? <EyeOff size={20} /> : <Eye size={20} />}
-              </span>
+          <div className="flexs-container">
+            <div className="flex-container">
+              <div className="form-group">
+                <input
+                  placeholder="Вигадайте нікнейм"
+                  name="userName"
+                  className={hasError("UserName") ? "error-input" : ""}
+                  type="text"
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+                {errors.UserName && (
+                  <span className="text-danger">{errors.UserName[0]}</span>
+                )}
+              </div>
+              <div className="form-group">
+                <input
+                  type="text"
+                  name="email"
+                  className={hasError("Email") ? "error-input" : ""}
+                  placeholder="example@mail.com"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                {errors.Email && (
+                  <span className="text-danger">{errors.Email[0]}</span>
+                )}
+              </div>
             </div>
 
-            {errors.ConfirmPassword && (
-              <span className="text-danger">{errors.ConfirmPassword[0]}</span>
-            )}
-          </div>
+            <div className="separator">
+              <span></span>
+            </div>
 
+            <div className="flex-container">
+              <div className="form-group">
+                <div className="password-container">
+                  <input
+                    type={showPassword ? "text" : "password"}
+                    name="password"
+                    className={hasError("Password") ? "error-input" : ""}
+                    placeholder="Пароль"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                  <span
+                    className="toggle-password"
+                    onClick={() => setShowPassword(!showPassword)}
+                  >
+                    {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
+                  </span>
+                </div>
+                {errors.Password && (
+                  <span className="text-danger">{errors.Password[0]}</span>
+                )}
+              </div>
+              <div className="form-group">
+                <div className="password-container">
+                  <input
+                    type={showConfirmPassword ? "text" : "password"}
+                    name="confirm"
+                    className={hasError("ConfirmPassword") ? "error-input" : ""}
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                    placeholder="Повторіть пароль"
+                  />
+                  <span
+                    className="toggle-password"
+                    onClick={() => setShowConfirmPassword(!showConfirmPassword)}
+                  >
+                    {showConfirmPassword ? (
+                      <EyeOff size={20} />
+                    ) : (
+                      <Eye size={20} />
+                    )}
+                  </span>
+                </div>
+
+                {errors.ConfirmPassword && (
+                  <span className="text-danger">
+                    {errors.ConfirmPassword[0]}
+                  </span>
+                )}
+              </div>
+            </div>
+          </div>
+          
           <button
             type="submit"
             className="btn-auth"
