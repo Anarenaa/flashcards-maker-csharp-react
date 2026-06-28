89fc5b97a0b468cbfe587ff7e3624401c3d89316
Anastasiia Mikulych <mika.akima.22@gmail.com>
2026-06-28 16:14:47 +0300

    Add autorouting depending on AuthToken

---
+++ backend/WebAPI/Controllers/BaseApiController.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class BaseApiController : ControllerBase
+    {
+        protected int UserId
+        {
+            get
+            {
+                var userIdStr = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+                return int.TryParse(userIdStr, out int userId) ? userId : 0;
+            }
+        }
+    }
+}
---
+++ frontend/src/pages/MainPage.jsx
@@ -0,0 +1,7 @@
+import './MainPage.scss';
+
+export default function MainPage(){
+    return(
+        <h1>Hi</h1>
+    );
+}
