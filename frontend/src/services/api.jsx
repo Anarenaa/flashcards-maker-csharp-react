10125f2148c2ce56c7b8ecda08eb42a54f3e54c9
Anastasiia Mikulych <mika.akima.22@gmail.com>
2026-09-13 20:51:12 +0300

    Update baseURL for production

--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import toast from "react-hot-toast";
 
 const api = axios.create({
-  baseURL: "/api", // vite.config.js
+  baseURL: import.meta.env.VITE_API_URL || '/api', // vite.config.js
   withCredentials: true, // cookies
 });
 
